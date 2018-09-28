17ac11611ff86e8871fcb66e3471047074437acc
Nic Horstmeier <nhorstmeier@dynamicagency.com>
2018-09-28 11:23:03 -0500

    REFACTOR add inherit checkbox in cms

--- src/Extensions/ElementalSetSiteTreeExtension.php
+++ src/Extensions/ElementalSetSiteTreeExtension.php
@@ -2,6 +2,8 @@
 
 namespace Dynamic\ElementalSets\Extensions;
 
+use SilverStripe\Forms\CheckboxField;
+use SilverStripe\Forms\FieldList;
 use SilverStripe\ORM\DataExtension;
 
 /**
@@ -11,10 +13,22 @@ use SilverStripe\ORM\DataExtension;
 class ElementalSetSiteTreeExtension extends DataExtension
 {
     private static $db = [
-        'InheritElemenalSets' => 'Boolean',
+        'InheritElementalSets' => 'Boolean',
     ];
-    
+
     private static $defaults = [
         'InheritElementalSets' => 1,
     ];
+
+    /**
+     * @param FieldList $fields
+     */
+    public function updateCMSFields(FieldList $fields)
+    {
+        $fields->addFieldToTab(
+            'Root.ElementSets',
+            CheckboxField::create('InheritElementalSets')
+                ->setTitle('Inherit Elements from Elemental Sets')
+        );
+    }
 }
